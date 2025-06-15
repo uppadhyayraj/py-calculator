6a672d5d2d7721ad9bae0370dedbccfaa21276a5
RajUppadhyay <uppadhyayraj@gmail.com>
2025-06-15 23:29:55 +1200

    test error

--- tests/operations/test_subtraction.py
+++ tests/operations/test_subtraction.py
@@ -5,4 +5,4 @@ def test_subtraction():
     assert Subtraction.execute(5, 3) == 2
     assert Subtraction.execute(1, 1) == 0
     assert Subtraction.execute(0, 5) == -5
-    assert Subtraction.execute(2.5, 1.0) == 1.5
+    assert Subtraction.execute(2.5, 1.0) == 3.5
